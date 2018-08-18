b31c900f632fc2c2f157b11d241cbaf0b6e4815b
Blackdogman <superkidchy@qq.com>
2018-08-18 17:18:28 +0800

    shi'a

---
+++ orangecloud-api/src/main/java/net/orangecloud/api/MainTest.java
@@ -0,0 +1,11 @@
+package net.orangecloud.api;
+
+import net.orangepeels.common.constants.*;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        WeekDaysConstants weekDaysConstants = new WeekDaysConstants();
+        System.out.println("Friday's code = " + weekDaysConstants.FRIDAY);
+    }
+}
---
+++ orangecloud-common/src/main/java/net/orangepeels/common/constants/WeekDaysConstants.java
@@ -0,0 +1,14 @@
+package net.orangepeels.common.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WeekDaysConstants {
+    public final Integer MONDAY = 0;
+    public final Integer TUESDAY = 1;
+    public final Integer WEDNESDAY = 2;
+    public final Integer THURSDAY = 3;
+    public final Integer FRIDAY = 4;
+    public final Integer SATURDAY = 5;
+    public final Integer SUNDAY = 6;
+}
